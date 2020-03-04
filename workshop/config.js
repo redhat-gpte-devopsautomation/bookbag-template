3009bf606ec2b51f113a9f25f427e18af1617de1
Pavel Anni <panni@redhat.com>
2020-03-04 15:49:10 -0500

    Add build and deploy templates from JK

--- workshop/config.js
+++ workshop/config.js
@@ -1,8 +1,27 @@
-function initialize(workshop) {
-            workshop.load_workshop();
+var fs = require('fs');
 
-                workshop.data_variable('GUID', process.env['GUID']);
-                workshop.data_variable('USERNAME', process.env['USERNAME']);
+function initialize(workshop) {
+    workshop.load_workshop();
+
+    try {
+        files = fs.readdirSync('/var/run/workshop-vars')
+        for (var i=0; i < files.length; i++) {
+            console.log(files[i])
+            if (files[i].match(/^[a-zA-Z_][a-zA-Z0-9_]+$/)) {
+                console.log(files[i])
+                workshop.data_variable(files[i], fs.readFileSync('/var/run/workshop-vars/' + files[i], 'utf8'));
+	    }
+        }
+    } catch(err) {
+        console.log(err)
+    }
+
+    try {
+        workshop_vars = JSON.parse(process.env['WORKSHOP_VARS'])
+        for (var varname in workshop_vars) {
+            workshop.data_variable(varname, workshop_vars[varname]);
+        }
+    } catch(err) {}
 }
 
 exports.default = initialize;
